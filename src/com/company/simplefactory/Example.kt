4fad85ecfd8245e92b230130a5a229c1bed3d6e7
SANGRAME <rajat.sangrame@MP-C02FW5NSMD6R.local>
2021-10-12 11:54:37 +0530

    simple factory

---
+++ src/com/company/simplefactory/Example.kt
@@ -0,0 +1,44 @@
+package com.company.simplefactory
+
+import java.lang.RuntimeException
+
+class Rectangle : Shape {
+    override fun draw() {
+        println("Drawing Rectangle")
+    }
+}
+
+class Circle : Shape {
+    override fun draw() {
+        println("Drawing Circle")
+    }
+}
+
+class Square : Shape {
+    override fun draw() {
+        println("Drawing Square")
+    }
+}
+
+interface Shape {
+    fun draw()
+}
+
+class ShapeFactory {
+
+    companion object {
+        fun getShape(type: String): Shape {
+            return when (type) {
+                "Circle" -> Circle()
+                "Square" -> Square()
+                "Rectangle" -> Rectangle()
+                else -> throw RuntimeException("Invalid Shape")
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+
+    ShapeFactory.getShape("Circle").draw()
+}
